905023c23ff832261ee27c128f73fd29ea33594a
rakibjrb <rakibjrb572157@gmail.com>
2024-01-28 20:25:23 +0600

    added car dealers section and completed top section with responsive

--- car-dealer/src/Layout/Main/Main.jsx
+++ car-dealer/src/Layout/Main/Main.jsx
@@ -1,10 +1,12 @@
 import Banner from "../../Pages/Home/Banner/Banner";
+import Dealer from "../../Pages/Home/Dealer/Dealer";
 
 const Main = () => {
   return (
-    <>
+    <div className="space-y-24 lg:space-y-32">
       <Banner />
-    </>
+      <Dealer />
+    </div>
   );
 };
 
---
+++ car-dealer/src/Pages/Home/Dealer/Dealer.jsx
@@ -0,0 +1,23 @@
+const Dealer = () => {
+  return (
+    <div className="max-w-screen-2xl mx-auto px-3 2xl:px-0">
+      <h2 className="text-xl uppercase font-semibold text-red-600 border-b border-red-500 border-dotted inline-block">
+        20% off for online booking
+      </h2>
+      <div className="mt-4 mb-10 flex flex-col md:flex-row space-y-8 md:space-y-0">
+        <div className="flex-1">
+          <h2 className="capitalize text-5xl font-bold xl:w-3/4 2xl:w-2/3">
+            We, Re Trusted your car <span className="text-red-600">dealer</span>
+          </h2>
+        </div>
+        <p className="flex-1 text-xl pl-4 border-l-4 border-red-600 w-full">
+          For 15 years, we raising the standerd of used car retailing with one
+          of the most innovative reliable used programmes ever created. A
+          comprehesive range of
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Dealer;
