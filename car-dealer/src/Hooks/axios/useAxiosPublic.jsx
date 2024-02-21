d3d88fe8b7a4bba2aad16ca53c19e5a7992357be
rakibjrb <rakibjrb572157@gmail.com>
2024-02-21 15:38:49 +0600

    site hosted to firebase

--- car-dealer/src/Hooks/axios/useAxiosPublic.jsx
+++ car-dealer/src/Hooks/axios/useAxiosPublic.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 const useAxiosPublic = () => {
   const instance = axios.create({
     baseURL: `${import.meta.env.VITE_Server_Url}/api`,
+    withCredentials: true,
   });
   return instance;
 };
