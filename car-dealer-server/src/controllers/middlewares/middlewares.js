99aae507a61a9476db9bbd927ddd7626564cefc5
rakibjrb <rakibjrb572157@gmail.com>
2024-02-23 14:44:37 +0600

    updated middleware function on server side

--- car-dealer-server/src/controllers/middlewares/middlewares.js
+++ car-dealer-server/src/controllers/middlewares/middlewares.js
@@ -1,8 +1,14 @@
+require("dotenv").config();
 const cors = require("cors");
 
 const middlewares = (express, app) => {
   app.use(express.json());
-  app.use(cors());
+  app.use(
+    cors({
+      origin: process.env.Origin,
+      credentials: true,
+    })
+  );
 };
 
 module.exports = middlewares;
