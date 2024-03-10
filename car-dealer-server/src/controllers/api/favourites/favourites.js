e397164356d33f2462b7c13cbc69d4179be28694
Rakibjrb <rakibjrb572157@gmail.com>
2024-03-10 10:49:01 +0600

    added routes for set favourite items for user

--- car-dealer-server/src/controllers/api/favourites/favourites.js
+++ car-dealer-server/src/controllers/api/favourites/favourites.js
@@ -1,8 +1,21 @@
+const Favourites = require("../../../models/favourites/favourites");
+
 const saveFavourites = async (req, res, next) => {
   try {
     const data = req.body;
-    console.log(data);
-    res.send(data);
+    const inDB = await Favourites.findOne({
+      itemId: data.itemId,
+      useremail: data.useremail,
+    });
+    if (inDB) {
+      res.send({
+        status: 403,
+        message: "Already Added",
+      });
+      return;
+    }
+    const savedFavourite = await Favourites.create(data);
+    res.send(savedFavourite);
   } catch (error) {
     next(error);
   }
---
+++ car-dealer-server/src/models/favourites/favourites.js
@@ -0,0 +1,11 @@
+const { model, Schema } = require("mongoose");
+
+const favouritesSchema = new Schema({
+  itemId: { type: String, required: true },
+  image: { type: String, required: true },
+  title: { type: String, required: true },
+  useremail: { type: String, required: true },
+});
+
+const Favourites = model("Favourites", favouritesSchema, "favourites");
+module.exports = Favourites;
